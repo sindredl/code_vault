d40bbce146f131923192ac9b5790205efb08a862
mNygaard <martiny1991@gmail.com>
2013-09-25 10:49:18 +0200

    A couple of new files

---
+++ example14.py
@@ -0,0 +1,21 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Copying from %s to %s" % (from_file, to_file)
+
+in_file = open(from_file)
+indata = in_file.read()
+
+print "The input file is %d bytes long" % len(indata)
+
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print "Alright, all done."
+
+# This is not necessary, but more or less done to have a cleaner code.
+out_file.close()
+in_file.close()
