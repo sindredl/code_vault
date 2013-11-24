40e7ccd259c28f05c6ec14186ab2a033b889581e
mNygaard <martiny1991@gmail.com>
2013-11-24 21:02:09 +0100

    Rearranged the files and finished ex22

---
+++ Assignment 2/example22.py
@@ -0,0 +1,7 @@
+Code/Symbol     || 		Name             ||            		Functions
+                ||                       || 
+print           ||      print			 ||				    Prints a text/string.
+#               ||      Hashtag/Pound    ||                 When entered text becomes a comment. 
+def             ||      Function 		 ||					Defines a function is defined followed by name and parentheses.
+return          ||      Return           ||                 Returns a given value.
+import          ||      Import           ||					Imports and adds features to the file.
